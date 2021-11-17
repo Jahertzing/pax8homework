357af60fb3860f3a1f1126d8e8fa35a010491bd8
Jahertzing <jahertzing@gmail.com>
2021-11-17 16:44:27 -0700

    Including new edit and viewcompany files

---
+++ src/views/ViewCompany.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>{{company}}</h1>
+</template>
+
+<script>
+import CompanyService from '../services/CompanyService';
+export default {
+  name: 'ViewCompany',
+  data() {
+    return {
+      company: null,
+      companyFailedToLoad: false,
+    };
+  },
+  created() {
+    const companyIdParam = this.$route.params.companyId;
+    const companyId = parseInt(companyIdParam);
+    if (!isNaN(companyId)) {
+      CompanyService.getById(companyId).then((data) => {
+        this.company = data;
+      }).catch(() => {
+        this.companyFailedToLoad = true;
+      });
+    } else {
+      this.companyFailedToLoad = true;
+    }
+  }
+};
+</script>
